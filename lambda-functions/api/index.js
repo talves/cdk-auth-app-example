2d52fd72e3461fea02be771b901a1299d1d48db5
talves <tony@3alves.com>
2021-02-24 14:30:46 -0800

    api

---
+++ lambda-functions/api/index.js
@@ -0,0 +1,11 @@
+const { createServer, proxy } = require("aws-serverless-express");
+const { Context } = require("aws-lambda");
+const { expressApp } = require("./express-app");
+
+const server = createServer(expressApp);
+
+// noinspection JSUnusedGlobalSymbols
+const handler = (event /*: any */, context /*: Context */) =>
+  proxy(server, event, context);
+
+module.exports.handler = handler;
